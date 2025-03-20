5a0e47f610bc58a27f9f07cc1d304059f90fbfaf
ShyamSaran Rajendran <shyamsaranr.22cse@kongu.edu>
2025-03-20 15:27:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u saran006 -p dckr_pat_Epa7MSeqL5igNF0BAROJHOt7dys
+    docker tag test1 saran006/test1
+    docker push saran006/test1
+    # docker-compose up -d
     
